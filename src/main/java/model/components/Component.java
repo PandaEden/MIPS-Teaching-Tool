cb16d1a879eb5dfbe86196cc0434bc10c544365a
Adnaan Hussain <hussaa65@aston.ac.uk>
2021-04-13 22:10:59 +0100

    Component ALU tested and implemented, and modified RegisterBank noAction
    Register Bank noAction would clear  lastRead and lastWritten.
    
    now when read is to perform no action, it prints NoRead instead.
    
    NoAction will be phased out soon

--- src/main/java/model/components/Component.java
+++ src/main/java/model/components/Component.java
@@ -17,12 +17,47 @@ public class Component {
 	 <p>If either input is null, addition is not attempted, null is forwarded
 	 <p>And the message is not logged. */
 	@Nullable
-	public static Integer Adder(Integer input0, Integer input1,@NotNull String message, @NotNull ExecutionLog log){
+	public static Integer Adder(Integer input0, Integer input1, @NotNull String message, @NotNull ExecutionLog log){
 		if ( input0==null || input1==null )
 			return null;
 		
 		log.appendEx(message);
 		return input0+input1;
+	}
+	
+	/** Performs ALU operation based on 4-bit ALU_OP, The last Bit is BInvert
+	 <p><b>If the first (leading) Bit is 1, it Inverts the result.</b> I.E 00010</p>
+	 
+	 
+	 */
+	public static int ALU(@NotNull Integer input0, @NotNull Integer input1, @NotNull String ALU_OP, @NotNull ExecutionLog log){
+		int output; // TODO remove null
+		// BInvert is determined by bit[0], SUB/SLT
+		
+		String sign = "+";
+		switch (ALU_OP.toUpperCase()){
+			case "NOP": // -- do nothing, for Nop & Exit
+				return input0;
+			case "ADD": // ADD
+				output=input0 + input1; sign = " + ";
+				break;
+			case "SUB": // SUB
+				output=input0 - input1; sign = " - ";
+				break;
+			case "SLL": //Shift Left Logical
+				String biteStream = Integer.toBinaryString(input1);
+				if (biteStream.length()>5)
+					input1 = Integer.parseInt(biteStream.substring(biteStream.length()-5), 2);
+				output=input0 << input1; sign = " << ";
+				break;
+			case "SLT": //Set Less Than
+				output=input0 < input1?1:0; sign = " set-on < ";
+				break;
+			default:
+				throw new IllegalStateException("ALU_OP ["+ALU_OP+"] Not Implemented!");
+		}
+		log.append( "\tResult = " + input0 + sign + input1 + " ==> " + output );
+		return output;
 	}
 	
 }
--- src/main/java/model/components/RegisterBank.java
+++ src/main/java/model/components/RegisterBank.java
@@ -14,16 +14,17 @@ import util.logs.Logger;
  */
 public class RegisterBank {
 	public static RegFormat regFormat=RegFormat.R;
+	public static boolean fmtUpperCase=true;
 	private final int[] registers;
 	private final ExecutionLog executionLog;
 	private final String NAME="RegisterBank";
 	private Integer LAST_READ0=null;
 	private Integer LAST_READ1=null;
-	private Integer LAST_WRITTEN=null;
 	
 	//TODO Add Named versions of Read/Write, so the Operand Name [RD/RS/RT] can be printed to the log with the action.
+	private Integer LAST_WRITTEN=null;
 	
-	public RegisterBank(int[] registers, @NotNull ExecutionLog executionLog) {
+	public RegisterBank (int[] registers, @NotNull ExecutionLog executionLog) {
 		if ( registers.length!=32 )
 			throw new IllegalArgumentException( "Register Bank Must be 32 indexes!" );
 		if ( registers[ 0 ]!=0 )
@@ -33,30 +34,25 @@ public class RegisterBank {
 		this.registers=registers;
 		this.executionLog=executionLog;
 	}
-	
 	/**
 	 Reads the data of the register at the given index, Returns 0 for null input.
 	 
 	 @throws IndexOutOfBoundsException if register index out of bounds.
 	 */
-	public int read(@Nullable Integer index) throws IndexOutOfBoundsException {
+	public int read (@Nullable Integer index) throws IndexOutOfBoundsException {
 		LAST_READ1=null;
 		int val=0;
 		
 		if ( index==null ) {
 			LAST_READ0=null;
-			noAction( );
+			this.executionLog.append( NAME + ":\t" + "No Read!" );
 		} else if ( inRange( index ) ) {
 			LAST_READ0=index;
-			val=this.registers[ index ];
-			
-			this.executionLog.append( NAME + ":\t" + "Reading Value[" + val + "]\tFrom Register Index["
-									  + fmtReg( index ) + "]!" );
+			val=readVal( index );
 		}
 		
 		return val;
 	}
-	
 	/**
 	 Reads the data of the registers at both given index, Returns 0 for null input.
 	 
@@ -69,13 +65,13 @@ public class RegisterBank {
 	 @see Convert#r2Named(String)
 	 @see Convert#namedRegisters
 	 */
-	public int[] read(@Nullable Integer index0, @Nullable Integer index1) throws IndexOutOfBoundsException {
+	public int[] read (@Nullable Integer index0, @Nullable Integer index1) throws IndexOutOfBoundsException {
 		int data0=0, data1=0;
 		
 		if ( index0==null && index1==null ) {
 			LAST_READ0=null;
 			LAST_READ1=null;
-			noAction( );
+			this.executionLog.append( NAME + ":\t" + "No Read!" );
 			
 		} else if ( index0!=null && index1==null ) {
 			data0=read( index0 );
@@ -84,22 +80,29 @@ public class RegisterBank {
 			LAST_READ0=null;
 			LAST_READ1=index1;
 			if ( inRange( index1 ) ) {
-				data1=this.registers[ index1 ];
-				this.executionLog.append( NAME + ":\t" + "Reading Value[" + data1 + "]\tFrom Register Index["
-										  + fmtReg( index1 ) + "]!" );
+				data1=readVal( index1 );
 			}
 		} else { // index0 != null && index1 != null
 			LAST_READ0=index0;
 			LAST_READ1=index1;
 			if ( inRange( index0 ) && inRange( index1 ) ) {
-				data0=this.registers[ index0 ];
-				data1=this.registers[ index1 ];
-				this.executionLog.append( NAME + ":\t" + "Reading Values[" + data0 + ", " + data1
-										  + "]\tFrom Register Indexes[" + fmtReg( index0 ) + ", "
-										  + fmtReg( index1 ) + "]!" );
+				data0=readVal( index0 );
+				data1=readVal( index1 );
+//				this.executionLog.append( NAME + ":\t" + "Reading Values[" + colorVal( index0, data0 ) + ", "
+//										  + colorVal( index1, data1 )
+//										  + "]\tFrom Register Indexes[" + fmtReg( index0 ) + ", "
+//										  + fmtReg( index1 ) + "]!" );
 			}
 		}
 		return new int[] { data0, data1 };
+	}
+	private int readVal(int index){
+		inRange( index );
+		
+		int data = this.registers[ index ];
+		this.executionLog.append( NAME + ":\t" + "Reading Value[" + data
+								  + "]\tFrom Register Index[" + fmtReg( index ) + "]!" );
+		return data;
 	}
 	
 	
@@ -115,30 +118,25 @@ public class RegisterBank {
 	 @see Convert#r2Named(String)
 	 @see Convert#namedRegisters
 	 */
-	@SuppressWarnings ("UnusedReturnValue")
-	public boolean write(@Nullable Integer index, @Nullable Integer data) throws IndexOutOfBoundsException {
+	@SuppressWarnings ( "UnusedReturnValue" )
+	public boolean write (@Nullable Integer index, @Nullable Integer data) throws IndexOutOfBoundsException {
 		if ( index==null || data==null || index==0 ) {
 			LAST_WRITTEN=null;
-			noAction( );
+			this.executionLog.append( NAME + ":\t" + "No Write!" );
 			return false;
 		} else if ( inRange( index ) ) {
 			LAST_WRITTEN=index;
+			
+			LAST_READ0=(LAST_READ0==LAST_WRITTEN) ? null : LAST_READ0; // if read == written, clear read
+			LAST_READ1=(LAST_READ1==LAST_WRITTEN) ? null : LAST_READ1; // to avoid read colour being printed
+			
 			this.registers[ index ]=data;
-			this.executionLog.append( NAME + ":\t" + "Writing Value[" + data + "]\tTo Register Index["
+			this.executionLog.append( NAME + ":\t" + "Writing Value[" + colorVal( index, data ) + "]\tTo Register Index["
 									  + fmtReg( index ) + "]!" );
 		}
 		return true;
 	}
-	
-	/** Explicit instruction to do nothing. And Clears the LAST_WRITTEN/READ */
-	public void noAction() {
-		LAST_WRITTEN=null;
-		LAST_READ1=null;
-		LAST_READ0=null;
-		this.executionLog.append( NAME + ":\t" + "No Action!" );
-	}
-	
-	public boolean inRange(int index) throws IndexOutOfBoundsException {
+	public boolean inRange (int index) throws IndexOutOfBoundsException {
 		int MIN_INDEX=0;
 		int MAX_INDEX=31;
 		if ( index>=MIN_INDEX && index<=MAX_INDEX )
@@ -197,15 +195,21 @@ public class RegisterBank {
 			default:
 				break;
 		}
-		return rtn + reg.toUpperCase( );
+		return rtn + (fmtUpperCase ? reg.toUpperCase( ) : reg);
 	}
-	
+	/** Explicit instruction to do nothing. And Clears the LAST_WRITTEN/READ */
+	public void noAction ( ) {
+		LAST_WRITTEN=null;
+		LAST_READ1=null;
+		LAST_READ0=null;
+		this.executionLog.append( NAME + ":\t" + "No Action!" );
+	}
 	/**
 	 Returns a formatted string, that when printed, displays the current state of the register bank.
 	 <p><b>Use with print(</b><i> registerBank.format() </i><b>)</b>
 	 */
 	@NotNull
-	public String format() {
+	public String format ( ) {
 		StringBuilder rtn=new StringBuilder( "-------- -------- -------- REGISTER-BANK -------- -------- -------- -------- \n" );
 		int I1=0, I2=4, I3=8, I4=12, I5=16, I6=20, I7=24, I8=28;
 		
@@ -219,12 +223,11 @@ public class RegisterBank {
 			   .append( "\t" ).append( fmtRegWithData( I6++ ) )
 			   .append( "\t" ).append( fmtRegWithData( I7++ ) )
 			   .append( "\t" ).append( fmtRegWithData( I8++ ) )
-			.append( "|\n" );
+			   .append( "|\n" );
 		}
 		rtn.append( "-------- -------- -------- ---- --- ---- -------- -------- -------- -------- \n" );
 		return rtn.toString( );
 	}
-	
 	/** Formats the register depending on {@link RegFormat}, and combines with the value at that register */
 	private String fmtRegWithData(int index) {
 		return colorReg( index, regName( index ) + ": " + registers[ index ] );
